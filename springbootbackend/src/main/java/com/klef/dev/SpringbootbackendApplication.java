cde3c0720af23ebef92694f10cb73648588f42de
DeepikaReddy <deepikareddymandapati@gmail.com>
2025-10-23 20:59:58 +0530

    v1.0

---
+++ springbootbackend/src/main/java/com/klef/dev/SpringbootbackendApplication.java
@@ -0,0 +1,23 @@
+package com.klef.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SpringbootbackendApplication extends SpringBootServletInitializer  
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SpringbootbackendApplication.class, args);
+		System.out.println("Project Backend is Running Successfully ....");
+	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinsBackendApplication.class);
+    }
+
+
+}
